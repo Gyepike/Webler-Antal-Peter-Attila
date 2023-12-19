9b3bfd134eee9812f3b862840c1e54083b515c9b
gyepike <antipet@gmail.com>
2023-12-19 15:45:15 +0100

    first commit

---
+++ src/Animal.java
@@ -0,0 +1,58 @@
+class Allatok {
+    String nev;
+
+    public Allatok(String nev) {
+        this.nev = nev;
+    }
+
+    public void hangot_kiad() {
+        System.out.println("Allatok kozos ose");
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    @Override
+    public String toString() {
+        return "Allatok{" +
+                "nev='" + nev + '\'' +
+                '}';
+    }
+}
+
+class Kutya extends Allatok {
+    public Kutya(String nev) {
+        super(nev);
+    }
+
+    public void ugat() {
+        System.out.println("Vau Vau");
+    }
+}
+
+class Macska extends Allatok {
+    public Macska(String nev) {
+        super(nev);
+    }
+
+    public void nyavog() {
+        System.out.println("Miauuuuuu");
+    }
+}
+// itt minden Allat tipusnak megfelelo hangott ad ki a kutya : Vau Vau MAcsak Miauuu
+public class Animal {
+    public static void main(String[] args) {
+
+        Allatok myAnimals = new Allatok("Mirci");
+        Kutya myDog = new Kutya("Lord");
+        Macska myCat = new Macska("Mirci");
+        myAnimals.hangot_kiad();
+        myDog.ugat();
+        myCat.nyavog();
+    }
+}
